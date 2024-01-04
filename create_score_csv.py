b927e66767b5abcc95a6a5917693d138c3dd8fc2
Alex H <alexhuft@gmail.com>
2024-01-04 12:15:46 -0700

    Updated from most up to date version I had locally

--- create_score_csv.py
+++ create_score_csv.py
@@ -10,6 +10,18 @@ from bs4 import BeautifulSoup
 
 
 HEADER = "year,corps_name,rank,score\n"
+# it's just easier to pull the last 10 years manually
+YEAR_URLS = ["https://www.dci.org/scores/final-scores/2013-dci-world-championship-finals",
+            "https://www.dci.org/scores/final-scores/2014-dci-world-championships-world-class-finals",
+            "https://www.dci.org/scores/final-scores/2015-dci-world-championship-world-class-finals",
+            "https://www.dci.org/scores/final-scores/2016-dci-world-championships-finals",
+            "https://www.dci.org/scores/final-scores/2017-dci-world-championship-finals",
+            "https://www.dci.org/scores/final-scores/2018-dci-world-championship-finals",
+            "https://www.dci.org/scores/final-scores/2019-dci-world-championship-finals",
+            "https://www.dci.org/scores/final-scores/2020-dci-world-championship-finals",
+            "https://www.dci.org/scores/final-scores/2021-dci-world-championship-finals",
+            "https://www.dci.org/scores/final-scores/2022-dci-world-championship-finals",
+            "https://www.dci.org/scores/final-scores/2023-dci-world-championship-finals"]
 
 
 def write_to_csv(score_year: int, score_list: list) -> None:
@@ -26,12 +38,9 @@ if __name__ == "__main__":
         file.write(HEADER)
 
     # get scores by year
-    for year in tqdm(range(2016, 2024, 1)): # 2016-2023
-        base_url: str = f"https://www.dci.org/scores/final-scores/{year}-dci-world-championship-finals"
-        if year == 2016:
-            base_url = base_url.replace("championship", "championships")
+    for year, url in tqdm(zip(range(2013, 2024, 1), YEAR_URLS), total=len(YEAR_URLS)): # 2016-2023
         # get webpage
-        request = requests.get(base_url, timeout=5)
+        request = requests.get(url, timeout=5)
         soup = BeautifulSoup(request.text, "html.parser")
         tr_sections: bs4.element.ResultSet = soup.find_all("tr") # get every section of scores
         for section in tr_sections:
@@ -40,4 +49,4 @@ if __name__ == "__main__":
             if part_list[0].isdigit():
                 write_to_csv(year, part_list)
 
-        sleep(5) # stop from getting found out
+        sleep(3) # stop from getting found out
